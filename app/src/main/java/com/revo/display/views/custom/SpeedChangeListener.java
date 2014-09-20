68ece99c295881287267ac22e4c919b52de3e7da
Chris <sihrc.c.lee@gmail.com>
2014-09-20 19:49:06 -0400

    ADD: Speedometer

---
+++ app/src/main/java/com/revo/display/views/custom/SpeedChangeListener.java
@@ -0,0 +1,7 @@
+package com.revo.display.views.custom;
+
+public interface SpeedChangeListener {
+	
+	public void onSpeedChanged(float newSpeedValue);
+
+}
--- app/src/main/java/com/revo/display/views/fragment/DriverFragment.java
+++ app/src/main/java/com/revo/display/views/fragment/DriverFragment.java
@@ -2,22 +2,22 @@ package com.revo.display.views.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.revo.display.R;
+import com.revo.display.views.custom.Speedometer;
 
 /**
  * Created by sihrc on 9/20/14.
  */
 public class DriverFragment extends Fragment {
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.driver_fragment, container);
+
+        Speedometer speedometer = (Speedometer) rootView.findViewById(R.id.speedometer);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
