596d071a5fe1d8d21d7216b3e6b08771884c9f51
Chris <sihrc.c.lee@gmail.com>
2015-01-21 22:17:17 -0500

    ADD: Developer Fragment Console View

--- app/src/main/java/com/revo/display/views/fragment/DriverFragment.java
+++ app/src/main/java/com/revo/display/views/fragment/DriverFragment.java
@@ -1,7 +1,5 @@
 package com.revo.display.views.fragment;
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,7 +9,6 @@ import android.view.ViewGroup;
 import com.firebase.client.Firebase;
 import com.revo.display.R;
 import com.revo.display.RevoApplication;
-import com.revo.display.bluetooth.DataReceiver;
 import com.revo.display.bluetooth.ValuesCallback;
 import com.revo.display.network.RFirebase;
 import com.revo.display.network.ValueCallback;
@@ -26,61 +23,21 @@ public class DriverFragment extends RevoFragment {
     RFirebase ref = RevoApplication.app.getFireBaseHelper();
     Firebase firebaseRef = ref.getRef(new String[]{"driver"});
 
-    Activity activity;
-
     //Views for Display
     RSpeedometer RSpeedometer;
     RBatteryMeter RBatteryMeter;
-    boolean registered = false;
-    BroadcastReceiver receiver = new DataReceiver(new ValuesCallback() {
-        @Override
-        public void handleValues(byte[] values) {
-            // Get data from bluetooth chip & send to firebase
-            if (RSpeedometer != null && RBatteryMeter != null) {
-                RSpeedometer.setCurrentSpeed((float) values[0]);
-                RBatteryMeter.setCurrentCharge((float) values[1]);
-            }
-
-            // This should happen in bluetooth callback
-            if (firebaseRef != null) {
-                firebaseRef.child("charge").setValue(values[0]);
-                firebaseRef.child("speed").setValue(values[1]);
-            }
-        }
-    });
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        this.activity = activity;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.driver_fragment, container, false);
         RSpeedometer = (RSpeedometer) rootView.findViewById(R.id.speedometer);
         RBatteryMeter = (RBatteryMeter) rootView.findViewById((R.id.batterymeter));
 
-        updateMode();
         return rootView;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        updateMode();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (activity != null && receiver != null && registered) {
-            activity.unregisterReceiver(receiver);
-        }
         ref.deregisterListener(DriverFragment.class.getSimpleName() + "charge", new String[]{"driver", "charge"});
         ref.deregisterListener(DriverFragment.class.getSimpleName() + "speed", new String[]{"driver", "speed"});
     }
@@ -114,20 +71,40 @@ public class DriverFragment extends RevoFragment {
         // Get data from firebase
         ref.registerListener(DriverFragment.class.getSimpleName() + "charge", new String[]{"driver", "charge"}, new ValueCallback() {
             @Override
-            public void handleValue(long value) {
+            public void handleValue(Object value) {
                 if (RBatteryMeter != null) {
-                    RBatteryMeter.onValueChanged(value);
+                    RBatteryMeter.onValueChanged((Long) value);
                 }
             }
         });
 
         ref.registerListener(DriverFragment.class.getSimpleName() + "speed", new String[]{"driver", "speed"}, new ValueCallback() {
             @Override
-            public void handleValue(long value) {
+            public void handleValue(Object value) {
                 if (RSpeedometer != null) {
-                    RSpeedometer.onValueChanged(value);
+                    RSpeedometer.onValueChanged((Long) value);
                 }
             }
         });
+    }
+
+    @Override
+    public ValuesCallback getValuesCallback() {
+        return new ValuesCallback() {
+            @Override
+            public void handleValues(byte[] values) {
+                // Get data from bluetooth chip & send to firebase
+                if (RSpeedometer != null && RBatteryMeter != null) {
+                    RSpeedometer.setCurrentSpeed((float) values[0]);
+                    RBatteryMeter.setCurrentCharge((float) values[1]);
+                }
+
+                // This should happen in bluetooth callback
+                if (firebaseRef != null) {
+                    firebaseRef.child("charge").setValue(values[0]);
+                    firebaseRef.child("speed").setValue(values[1]);
+                }
+            }
+        };
     }
 }
