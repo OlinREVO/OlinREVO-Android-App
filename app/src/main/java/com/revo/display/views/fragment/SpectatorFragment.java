6125fc67ba27eb3c7d993eae5989fc581f8957e1
Isaac Getto <isaac.getto@gmail.com>
2015-01-31 15:26:53 -0500

    add google map to spectator view

--- app/src/main/java/com/revo/display/views/fragment/SpectatorFragment.java
+++ app/src/main/java/com/revo/display/views/fragment/SpectatorFragment.java
@@ -1,11 +1,14 @@
 package com.revo.display.views.fragment;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.MapView;
 import com.revo.display.R;
 import com.revo.display.RevoApplication;
 import com.revo.display.bluetooth.ValuesCallback;
@@ -19,11 +22,15 @@ import com.revo.display.views.custom.Compass;
 public class SpectatorFragment extends RevoFragment {
     private RFirebase ref = RevoApplication.app.getFireBaseHelper();
     private Compass compass;
+    private MapView map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.spectator_fragment, container, false);
         compass = (Compass) rootView.findViewById(R.id.compass);
+        map = (MapView) rootView.findViewById(R.id.map);
+
+        map.onCreate(savedInstanceState);
 
         updateMode();
         return rootView;
@@ -32,13 +39,33 @@ public class SpectatorFragment extends RevoFragment {
     @Override
     public void onPause() {
         super.onPause();
+        map.onPause();
         ref.deregisterListener(SpectatorFragment.class.getSimpleName() + "direction", new String[] {"driver", "direction"});
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        map.onResume();
         updateMode();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        map.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        map.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map.onLowMemory();
     }
 
     @Override
